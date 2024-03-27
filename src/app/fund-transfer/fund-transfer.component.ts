43589f951c338c1f5609664e70e4dc891ce07f58
vagadeeshwar <vagadeeshwar@gmail.com>
2024-03-27 13:57:06 +0530

    commit

--- src/app/fund-transfer/fund-transfer.component.ts
+++ src/app/fund-transfer/fund-transfer.component.ts
@@ -1,22 +1,35 @@
-// money-transaction.component.ts
-
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Transaction {
+  id: number;
+  type: string;
+  amount: number;
+  date: string;
+}
 
 @Component({
   selector: 'app-fund-transfer',
   templateUrl: './fund-transfer.component.html',
-  styleUrls: ['./fund-transfer.component.css']
+  styleUrl: './fund-transfer.component.css',
+  standalone: true,
+  imports: [CommonModule, FormsModule]
 })
+
 export class FundTransferComponent {
-  balance: number = 1000; // Starting balance
+  balance: number = 1500; // Starting balance
   amount: number = 0;
-  transactionType: string = 'withdraw'; // Default transaction type
+  count: number = 0;
+  transactionType: string = "withdraw"; // Default transaction type
+  transactionHistory: Transaction[] = [];
+
 
   // Method to perform transaction
   performTransaction(): void {
     if (this.transactionType === 'withdraw') {
       if (this.amount > this.balance) {
-        alert('Insufficient balance');
+        alert(`Insufficient balance: $${this.balance}`);
       } else {
         this.balance -= this.amount;
         alert(`Withdrawal successful. New balance: $${this.balance}`);
@@ -25,6 +38,14 @@ export class FundTransferComponent {
       this.balance += this.amount;
       alert(`Deposit successful. New balance: $${this.balance}`);
     }
+
+    this.transactionHistory.push({
+      id: ++this.count,
+      type: this.transactionType,
+      amount: this.amount,
+      date: new Date().toUTCString()
+    })
+
     this.amount = 0; // Reset amount after transaction
   }
 }
